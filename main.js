a01610b88d19e668f7845474132cd8e6f64b4e7f
LepkoQQ <lepko.san@gmail.com>
2023-03-19 00:33:45 +0100

    add scrolling text, move css to own file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-(async function main() {
+// rotate characters on hover
+(async function () {
   const characters = "abcdefghijklmnopqrstuvwxyz!#$%&/()=?€@[]{}§";
   const elements = Array.from(document.querySelectorAll(".rotate-characters"));
 
@@ -36,4 +37,35 @@
   elements.forEach((element) => {
     element.addEventListener("mouseover", rotateCharacters);
   });
+})();
+
+// scroll text rows
+(async function () {
+  const characters = "01";
+  const element = document.querySelector(".scroll-rows");
+
+  function fillRows() {
+    const height = Number.parseInt(window.getComputedStyle(element).height, 10);
+    const maxRows = (height / (11 * 1.5)) * 0.7;
+    element.innerText = "";
+
+    for (let i = 0; i < maxRows; i++) {
+      const row = document.createElement("div");
+      row.innerText = "c"
+        .repeat(50)
+        .split("")
+        .map((c) => characters[Math.floor(Math.random() * characters.length)])
+        .join("");
+      element.appendChild(row);
+    }
+  }
+
+  fillRows();
+  window.addEventListener("resize", () => {
+    fillRows();
+  });
+
+  setInterval(() => {
+    element.appendChild(element.firstElementChild);
+  }, 120);
 })();
