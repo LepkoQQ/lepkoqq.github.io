49c6dc2651b10b3f43438e8427b1cfd64669ff52
LepkoQQ <miha@lepko.net>
2026-06-20 19:34:27 +0200

    fix weather time update loop, display last weather update time and show a red outline if it fails

--- clock/clock.js
+++ clock/clock.js
@@ -53,73 +53,88 @@
 // update weather
 (async function () {
   async function updateWeather() {
-    const response = await fetch("https://api.tuhaz.zip/weather");
-    if (!response.ok) return;
+    try {
+      const response = await fetch("https://api.tuhaz.zip/weather");
+      if (!response.ok) return;
 
-    const data = await response.json();
-    const now = data.data.now;
+      const data = await response.json();
+      const now = data.data.now;
 
-    const weatherElement = document.querySelector(".weather");
+      const weatherElement = document.querySelector(".weather");
 
-    const iconEl = weatherElement.querySelector(".icon");
-    const iconImg = new Image();
-    iconImg.src = now.icon;
-    iconImg.onload = () => {
-      iconEl.innerHTML = "";
-      iconEl.appendChild(iconImg);
-    };
+      const iconEl = weatherElement.querySelector(".icon");
+      const iconImg = new Image();
+      iconImg.src = now.icon;
+      iconImg.onload = () => {
+        iconEl.innerHTML = "";
+        iconEl.appendChild(iconImg);
+      };
 
-    const temperatureEl = weatherElement.querySelector(".temperature");
-    temperatureEl.querySelector(".value").textContent = now.temperature.value;
-    temperatureEl.querySelector(".unit").textContent = now.temperature.unit;
+      const temperatureEl = weatherElement.querySelector(".temperature");
+      temperatureEl.querySelector(".value").textContent = now.temperature.value;
+      temperatureEl.querySelector(".unit").textContent = now.temperature.unit;
 
-    const humidityEl = weatherElement.querySelector(".humidity");
-    humidityEl.querySelector(".value").textContent = now.humidity.value;
-    humidityEl.querySelector(".unit").textContent = now.humidity.unit;
+      const humidityEl = weatherElement.querySelector(".humidity");
+      humidityEl.querySelector(".value").textContent = now.humidity.value;
+      humidityEl.querySelector(".unit").textContent = now.humidity.unit;
 
-    const pressureEl = weatherElement.querySelector(".pressure");
-    pressureEl.querySelector(".value").textContent = now.pressure.value;
-    pressureEl.querySelector(".unit").textContent = now.pressure.unit;
+      const pressureEl = weatherElement.querySelector(".pressure");
+      pressureEl.querySelector(".value").textContent = now.pressure.value;
+      pressureEl.querySelector(".unit").textContent = now.pressure.unit;
 
-    const windEl = weatherElement.querySelector(".wind");
-    let windSpeedValue = now.wind.speed.value;
-    if (now.wind.gusts.value) {
-      windSpeedValue += ` (${now.wind.gusts.value})`;
+      const windEl = weatherElement.querySelector(".wind");
+      let windSpeedValue = now.wind.speed.value;
+      if (now.wind.gusts.value) {
+        windSpeedValue += ` (${now.wind.gusts.value})`;
+      }
+      windEl.querySelector(".value").textContent = windSpeedValue;
+      windEl.querySelector(".unit").textContent = now.wind.speed.unit;
+      const windIconEl = windEl.querySelector(".icon");
+      const windIconImg = new Image();
+      windIconImg.src = now.wind.direction.icon;
+      windIconImg.onload = () => {
+        windIconEl.innerHTML = "";
+        windIconEl.appendChild(windIconImg);
+      };
+
+      {
+        const sunriseEl = weatherElement.querySelector(".sunrise");
+        const sunriseTime = new Date(now.sunrise);
+        const hrs = sunriseTime.getHours().toString().padStart(2, "0");
+        const mins = sunriseTime.getMinutes().toString().padStart(2, "0");
+        sunriseEl.querySelector(".hours .tens .value").textContent = hrs[0];
+        sunriseEl.querySelector(".hours .ones .value").textContent = hrs[1];
+        sunriseEl.querySelector(".minutes .tens .value").textContent = mins[0];
+        sunriseEl.querySelector(".minutes .ones .value").textContent = mins[1];
+      }
+
+      {
+        const sunsetEl = weatherElement.querySelector(".sunset");
+        const sunsetTime = new Date(now.sunset);
+        const hrs = sunsetTime.getHours().toString().padStart(2, "0");
+        const mins = sunsetTime.getMinutes().toString().padStart(2, "0");
+        sunsetEl.querySelector(".hours .tens .value").textContent = hrs[0];
+        sunsetEl.querySelector(".hours .ones .value").textContent = hrs[1];
+        sunsetEl.querySelector(".minutes .tens .value").textContent = mins[0];
+        sunsetEl.querySelector(".minutes .ones .value").textContent = mins[1];
+      }
+
+      {
+        const updatedEl = weatherElement.querySelector(".updated .time");
+        const updatedTime = new Date();
+        const updatedString = updatedTime.toLocaleTimeString("sl-SI", {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        updatedEl.textContent = `${updatedString}`;
+        weatherElement.style.outline = "";
+      }
+    } catch (error) {
+      weatherElement.style.outline = "1px solid #f00";
+    } finally {
+      const oneMinuteMs = 60 * 1000;
+      setTimeout(updateWeather, oneMinuteMs);
     }
-    windEl.querySelector(".value").textContent = windSpeedValue;
-    windEl.querySelector(".unit").textContent = now.wind.speed.unit;
-    const windIconEl = windEl.querySelector(".icon");
-    const windIconImg = new Image();
-    windIconImg.src = now.wind.direction.icon;
-    windIconImg.onload = () => {
-      windIconEl.innerHTML = "";
-      windIconEl.appendChild(windIconImg);
-    };
-
-    {
-      const sunriseEl = weatherElement.querySelector(".sunrise");
-      const sunriseTime = new Date(now.sunrise);
-      const hours = sunriseTime.getHours().toString().padStart(2, "0");
-      const minutes = sunriseTime.getMinutes().toString().padStart(2, "0");
-      sunriseEl.querySelector(".hours .tens .value").textContent = hours[0];
-      sunriseEl.querySelector(".hours .ones .value").textContent = hours[1];
-      sunriseEl.querySelector(".minutes .tens .value").textContent = minutes[0];
-      sunriseEl.querySelector(".minutes .ones .value").textContent = minutes[1];
-    }
-
-    {
-      const sunsetEl = weatherElement.querySelector(".sunset");
-      const sunsetTime = new Date(now.sunset);
-      const hours = sunsetTime.getHours().toString().padStart(2, "0");
-      const minutes = sunsetTime.getMinutes().toString().padStart(2, "0");
-      sunsetEl.querySelector(".hours .tens .value").textContent = hours[0];
-      sunsetEl.querySelector(".hours .ones .value").textContent = hours[1];
-      sunsetEl.querySelector(".minutes .tens .value").textContent = minutes[0];
-      sunsetEl.querySelector(".minutes .ones .value").textContent = minutes[1];
-    }
-
-    const oneMinuteMs = 60 * 1000;
-    setTimeout(updateWeather, oneMinuteMs);
   }
 
   updateWeather();
